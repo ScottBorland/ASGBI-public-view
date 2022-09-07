a3eac81a179ff8cee12fa91131d1ec4f695401b8
ScottBorland <scottborland0@gmail.com>
2022-09-07 14:48:06 +0100

    Remove commented out code

--- retrieveJSON.js
+++ retrieveJSON.js
@@ -1,13 +1,3 @@
-// const url='http:/localhost:3000';
-// var excelFile;
-
-// fetch(url)
-// .then(res=>{console.log(res)})
-// .catch(error=>console.log(error))
-
-// import myJson from './public/rows.json' assert {type: 'json'};
-
-// console.log(myJson)
 
 async function retrieveProgramme() {
     const res = await fetch('https://asgbi-programme.herokuapp.com', {
