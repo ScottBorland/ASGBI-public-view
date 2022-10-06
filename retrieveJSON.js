7530d0443486b143a761bd007b7b4be5ba453ff5
ScottBorland <scottborland0@gmail.com>
2022-10-06 20:49:41 +0100

    Change fetch req url to fly server

--- retrieveJSON.js
+++ retrieveJSON.js
@@ -1,6 +1,8 @@
+//Fly server: https://long-hill-1888.fly.dev/
 
+//Heroku server: https://asgbi-programme.herokuapp.com
 async function retrieveProgramme() {
-    const res = await fetch('https://asgbi-programme.herokuapp.com', {
+    const res = await fetch('https://long-hill-1888.fly.dev', {
       method: 'GET'
     });
     const data = await res.json();
